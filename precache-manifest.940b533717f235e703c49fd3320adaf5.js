448bbb1082ef379190c1c693c4e506e01030669e
ageha <ospac111@gmail.com>
2019-11-28 23:13:25 +0900

    Updates

---
+++ precache-manifest.940b533717f235e703c49fd3320adaf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da24012d62993dede263faf32c7e7cb",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "95e9eb6d55adbd1ac3a6",
+    "url": "/movie-app/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "a60557ab49ae9a27434e",
+    "url": "/movie-app/static/js/2.2544e078.chunk.js"
+  },
+  {
+    "revision": "95e9eb6d55adbd1ac3a6",
+    "url": "/movie-app/static/js/main.e21adca4.chunk.js"
+  },
+  {
+    "revision": "5ccce974657b8a913657",
+    "url": "/movie-app/static/js/runtime-main.1f64301b.js"
+  }
+]);
